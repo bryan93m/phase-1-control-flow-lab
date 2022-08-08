60094c04d304c802f89ac98f3ab5ceac856d6758
Bryan Meadows <bryan1993meadows@gmail.com>
2022-08-08 11:33:56 -0600

    commit tree a386b2788986c784e80a120570ee3717d7043ac1

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  let result;
+  if (number <= 400){
+    result = 'This one is on me!';
+  }
+  else if (number > 400 && number < 2000){
+    result = 'That will be twenty bucks.'
+  }
+  else if (number > 2000 && number < 2500){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  else if (number > 2500){
+    result = 'No can do.'
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+let result = (city == 'NYC') ? "Ok, sounds good." : "No go.";
+return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(e){
+ let result;
+ switch(e){
+  case 'generous':
+    result = 'Thank you so much.';
+    break;
+  case 'not as generous':
+    result = 'Thank you.';
+    break;
+  default:
+    result = 'Bye.';
+    break;
+ }
+ return result;
 }
